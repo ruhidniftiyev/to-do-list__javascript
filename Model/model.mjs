9876bec380e7a75593acc93d4cf25b5ce71ecf1f
ruhidnv <89708916+ruhidnv@users.noreply.github.com>
2022-04-13 18:13:46 +0300

    todo

---
+++ Model/model.mjs
@@ -0,0 +1,46 @@
+export default class Model {
+  constructor() {
+    this.taskArray = [];
+    this.inputsArray = [];
+  }
+
+  addToTaskArr(newTask) {
+    if (!newTask) return;
+    this.taskArray.push(newTask);
+  }
+
+  addtoInputsArr(obj) {
+    if (!obj) return;
+    this.inputsArray.push(obj);
+  }
+
+  resetTaskArray() {
+    this.taskArray = [];
+  }
+
+  removeFromTaskArr(newTask) {
+      this.inputsArray = this.inputsArray.filter(item => {
+          return item != newTask;
+      })
+  }
+
+  sortArrayDown() {
+    this.inputsArray.sort((a, b) => {
+      const aTag = a.firstChild.value;
+      const bTag = b.firstChild.value;
+      if (aTag > bTag) return 1;
+      if (aTag < bTag) return -1;
+      return 0;
+    });
+  }
+
+  sortArrayUp() {
+    this.inputsArray.sort((a, b) => {
+      const aTag = a.firstChild.value;
+      const bTag = b.firstChild.value;
+      if (aTag < bTag) return 1;
+      if (aTag > bTag) return -1;
+      return 0;
+    });
+  }
+}
